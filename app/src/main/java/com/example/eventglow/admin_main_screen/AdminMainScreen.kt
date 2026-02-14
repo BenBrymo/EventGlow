322c23bcbe0bdf69f9a7d9a16e1d096f0a4d435c
Ghost <benbrymo5@gmail.com>
2026-02-14 11:29:51 -1100

    Initial project setup

--- app/src/main/java/com/example/eventglow/admin_main_screen/AdminMainScreen.kt
+++ app/src/main/java/com/example/eventglow/admin_main_screen/AdminMainScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -496,9 +495,3 @@ private fun NavigationItem(
     }
 }
 
-
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    HomeScreen()
-}
